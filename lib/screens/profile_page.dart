ff4dfde505b577d8a94c3058d03ddac4b706781f
Trushang-Patel <22it116@charusat.edu.in>
2025-04-05 09:46:31 +0530

    Full Updated Profile Section

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,38 +1,189 @@
+import 'dart:io';
+import 'dart:typed_data'; // Import for Uint8List
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart'; // Import for kIsWeb
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image/image.dart' as img; // Import for image processing
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _dobController = TextEditingController();
+  final _educationLevelController = TextEditingController();
+  File? _profileImage;
+  String? _profileImageUrl;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  Future<void> _loadUserData() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final userData = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+      if (userData.exists) {
+        setState(() {
+          _usernameController.text = userData['username'] ?? '';
+          _dobController.text = userData['dob'] ?? '';
+          _educationLevelController.text = userData['educationLevel'] ?? '';
+          _profileImageUrl = userData['profileImageUrl'];
+        });
+      }
+    }
+  }
+
+  Future<void> _pickImage() async {
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      print('Picked file path: ${pickedFile.path}'); // Debugging: Log the file path
+      setState(() {
+        _profileImage = File(pickedFile.path);
+      });
+      await _uploadProfileImage();
+    } else {
+      print('No image selected.');
+    }
+  }
+
+  Future<void> _uploadProfileImage() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null && _profileImage != null) {
+      final storageRef = FirebaseStorage.instance.ref().child('profile_images/${user.uid}.jpg');
+
+      // Convert image to JPEG format
+      final imageBytes = await _profileImage!.readAsBytes();
+      final decodedImage = img.decodeImage(imageBytes);
+      final jpgImage = img.encodeJpg(decodedImage!);
+
+      await storageRef.putData(
+        Uint8List.fromList(jpgImage),
+        SettableMetadata(contentType: 'image/jpeg'),
+      );
+
+      final downloadUrl = await storageRef.getDownloadURL();
+      setState(() {
+        _profileImageUrl = downloadUrl;
+      });
+      await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
+        'profileImageUrl': _profileImageUrl,
+      }, SetOptions(merge: true));
+    }
+  }
+
+  Future<void> _saveUserData() async {
+    if (_formKey.currentState!.validate()) {
+      final user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
+          'username': _usernameController.text,
+          'dob': _dobController.text,
+          'educationLevel': _educationLevelController.text,
+        }, SetOptions(merge: true));
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Profile updated successfully!')),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Profile'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('assets/profile_placeholder.png'), // Replace with user's profile image
-            ),
-            SizedBox(height: 20),
-            Text(
-              'User Name', // Replace with user's name
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'user@example.com', // Replace with user's email
-              style: TextStyle(fontSize: 16, color: Colors.grey),
-            ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {
-                // Add logout functionality here
-              },
-              child: Text('Logout'),
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              GestureDetector(
+                onTap: _pickImage,
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundImage: _profileImage != null
+                      ? FileImage(_profileImage!)
+                      : (_profileImageUrl != null
+                          ? NetworkImage(_profileImageUrl!)
+                          : AssetImage('assets/profile_placeholder.png')) as ImageProvider,
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: Icon(Icons.camera_alt, color: Colors.white),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _usernameController,
+                decoration: InputDecoration(labelText: 'Username'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your username';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: _dobController,
+                decoration: InputDecoration(
+                  labelText: 'Date of Birth',
+                  hintText: 'YYYY-MM-DD',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your date of birth';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: _educationLevelController,
+                decoration: InputDecoration(labelText: 'Education Level'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your education level';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: _saveUserData,
+                child: Text('Save'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    await FirebaseAuth.instance.signOut(); // Sign out the user
+                    Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false); // Navigate to login page
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Error logging out: $e'),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                  }
+                },
+                child: Text('Logout'),
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+              ),
+            ],
+          ),
         ),
       ),
     );
